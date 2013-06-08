97a743e20752cd12fdb6ac9c341ea097f9ac7f17
marcogatti <marco.gatti.90@gmail.com>
2013-06-08 01:43:27 -0300

    Agregadas libreras de MailSystem

---
+++ Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ActiveUp;
+using ActiveUp.Net.Imap4;
+
+namespace glimpse.Models
+{
+    public class Class1
+    {
+    }
+}
