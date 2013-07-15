02b4271269a446cac1a533468329dcfcb8aa7251
marcogatti <marco.gatti.90@gmail.com>
2013-07-15 17:42:55 -0300

    Borrado del proyecto viejo y agregado del .sln

--- glimpse-v2.0/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="glimpse_v2._0.MvcApplication" Language="C#" %>
--- glimpse/Models/User.cs
+++ glimpse/Models/User.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace glimpse.Models
 {
     public class User
     {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
     }
 }
