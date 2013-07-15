b6238dcef2d3e105651b6b04ebc83ad8eda1eddb
galagus <galanteagustin66@gmail.com>
2013-07-15 19:44:26 -0300

    bootstrap style

--- glimpse/App_Start/BundleConfig.cs
+++ glimpse/App_Start/BundleConfig.cs
@@ -18,12 +18,19 @@ namespace glimpse
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scriptsbootstrap.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bootstrap/css").Include(
+                        "~/Content/bootstrap.css",
+                        "~/Content/bootstrap-responsive.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
