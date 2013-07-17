3b00a52b3c4467058aa97c6e60b2c69f3516ad08
GermanPaz <paz.german.i@gmail.com>
2013-07-17 19:46:23 -0300

    Creado el proyecto de Tests, con dos clases vacias para probar MailInterfaces

---
+++ glimpse.Tests/MailInterfaces/ConnectorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace glimpse.Tests.MailInterfaces
+{
+    [TestFixture]
+    public class ConnectorTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ glimpse.Tests/MailInterfaces/FetcherTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace glimpse.Tests
+{
+    [TestFixture]
+    public class FetcherTest
+    {
+        [Test]
+        public void ()
+        {
+        }
+    }
+}
