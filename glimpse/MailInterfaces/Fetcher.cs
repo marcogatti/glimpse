db0d10f43de504f9c22aee06754ea984f3842544
EzequielLopez <ezequiel.lopez.2009@hotmail.com>
2013-07-19 18:35:55 -0300

    Agrego métodos en el Fetcher para traer mails de un determinado mailbox (traer todos, traer los X ultimos, traer los X del medio). Agrego métodos en el Fetcher para seleccionar mailbox y cerrarlo. Agrego clase de exception para el Fetcher dentro de MailInterfacesExceptions. Formateo el texto a la' visual de las excepciones.

--- glimpse/MailInterfaces/Fetcher.cs
+++ glimpse/MailInterfaces/Fetcher.cs
@@ -4,35 +4,105 @@ using System.Linq;
 using System.Web;
 using ActiveUp.Net.Imap4;
 using ActiveUp.Net.Mail;
+using glimpse.Exceptions.MailInterfacesExceptions;
 
 namespace glimpse.MailInterfaces
 {
     public class Fetcher
     {
         private Imap4Client receiver;
+        private Mailbox currentOpenedMailbox;
 
         public Fetcher(String username, String password)
         {
             this.receiver = new Connector().Login(username, password);
+            this.currentOpenedMailbox = null;
         }
 
-        public MessageCollection GetInboxMails()
+        public MessageCollection getInboxMails()
         {
-            return this.GetMails("INBOX", "ALL");
+            return this.getMails("INBOX", "ALL");
         }
-
-        private MessageCollection GetMails(string mailBox, string searchPhrase)
+        private MessageCollection getMails(String mailBox, String searchPhrase)
         {
             Mailbox mails = this.receiver.SelectMailbox(mailBox);
             Int32[] localMailsID = mails.Search("ALL");
             MessageCollection messages = new MessageCollection();
 
-            for (int i = 0; i <= 1; i++)
+            for (int i = 0; i <= (localMailsID.Length -1); i++)
             {
-                messages.Add(mails.Fetch.MessageObject(localMailsID[i]));
+                //messages.Add(mails.Fetch.MessageObject(localMailsID[i]));
+                System.Diagnostics.Debug.WriteLine(mails.Fetch.MessageObject(localMailsID[i]).DateString);
             }
 
+            this.receiver.Close();
             return messages;
+        }
+
+        public MessageCollection getAllMailsFrom(String mailbox)
+        {
+            return this.getLastXMailsFrom(mailbox, this.getAmountOfMailsFrom(mailbox));
+        }
+        public MessageCollection getLastXMailsFrom(String mailbox, Int32 amountToRetrieve)
+        {
+            return this.getMiddleMailsFrom(mailbox,amountToRetrieve, 0);
+        }
+        public MessageCollection getMiddleMailsFrom(String mailbox, Int32 amountToRetrieve, Int32 startingMailOrdinal)
+        {
+            Mailbox targetMailbox = this.getMailbox(mailbox);
+            Int32 amountOfMails = targetMailbox.MessageCount;
+
+            if (amountToRetrieve > amountOfMails - startingMailOrdinal)
+            {
+                throw new MailReadingOverflowException("Se está pidiendo obtener más mails de los que posee el mailbox o leer más allá del último mail.");
+            }
+
+            MessageCollection mailsRetrieved = new MessageCollection();
+
+            //startingMailOrdinal: 0 representa el más reciente, a mayor valor, más antigüedad
+            for (Int32 currentMail = amountOfMails - startingMailOrdinal;
+                       currentMail > amountOfMails - startingMailOrdinal - amountToRetrieve;
+                       currentMail--)
+            {
+                mailsRetrieved.Add(targetMailbox.Fetch.MessageObject(currentMail));
+            }
+
+            return mailsRetrieved;
+        }
+        public Int32 getAmountOfMailsFrom(String mailbox)
+        {
+            Mailbox targetMailbox = this.getMailbox(mailbox);
+            return targetMailbox.MessageCount;
+        }
+
+        private Mailbox getMailbox(String targetMailboxName)
+        {
+            if (this.currentOpenedMailbox == null)
+            {
+                return this.openMailbox(targetMailboxName);
+            }
+            else
+            {
+                if (this.currentOpenedMailbox.ShortName == targetMailboxName)
+                {
+                    return this.currentOpenedMailbox;
+                }
+                else
+                {
+                    this.closeMailbox();
+                    return this.openMailbox(targetMailboxName);
+                }
+            }
+        }
+        private Mailbox openMailbox(String mailbox)
+        {
+            this.currentOpenedMailbox = this.receiver.SelectMailbox(mailbox);
+            return this.currentOpenedMailbox;
+        }
+        private void closeMailbox()
+        {
+            this.receiver.Close();
+            this.currentOpenedMailbox = null;
         }
 
         
