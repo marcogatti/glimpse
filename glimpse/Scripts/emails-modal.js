f3052763d83d95326be6e36ace0bd9930686fa0d
galagus <galanteagustin66@gmail.com>
2013-07-19 19:21:07 -0300

    modal mostrando emails

---
+++ glimpse/Scripts/emails-modal.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    $(".circle").on("click", function () {
+
+        var from = $('<h4>From: ' + $(this).data("from") + '</h4>');
+        var body = $('<p>' + $(this).data("body") + '</p>');
+        var subject = $('<h3>' + $(this).data("subject") + '</h3>');
+
+        $(".modal-body").find("h4").remove();
+        $(".modal-body").find("p").remove();
+        $(".modal-header").find("h3").remove();
+
+        $(".modal-body").append(from);
+        $(".modal-body").append(body);
+        $(".modal-header").append(subject);
+    })
+})
