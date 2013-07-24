b215215199939235ee9af9e66e6c2547567819c5
galagus <galanteagustin66@gmail.com>
2013-07-24 03:20:09 -0300

    interfaz mas pulida, posicionamiento mas consistente. las bolas se ajustan al tamanno INICIAL del contenedor, con la mas antigua en el borde derecho

--- glimpse/Scripts/emails-modal.js
+++ glimpse/Scripts/emails-modal.js
@@ -1,4 +1,25 @@
-﻿$(document).ready(function () {
+﻿
+function calculateEmailsPosition(maxAge) {
+
+    var containerWidth = $("#email-container").width();
+    var containerHeight = $("#email-container").height();
+
+    $(".circle").each(function () {
+
+        var left = $(this).attr('data-age') / maxAge;
+        var top = ($(this).attr('data-from').charCodeAt(0) - "a".charCodeAt(0)) / 26;    
+
+        $(this).css('top', function (index) {
+            return (top * containerHeight * 0.9) + 'px';
+        });
+
+        $(this).css('left', function (index) {
+            return (left * containerWidth * 0.9) + 'px';
+        });
+    })
+}
+
+$(document).ready(function () {
 
     $(".circle").on("click", function () {
 
@@ -13,29 +34,9 @@
         $(".modal-body").append(from);
         $(".modal-body").append(body);
         $(".modal-header").append(subject);
-    })
+    });
 
+    calculateEmailsPosition(maxAge);
 
-    //$(".circle").on("mouseenter", function () {
-    //    $(this).css('left', function (index) { return $(this).attr("data-age") + 'px' })
-    //})
 })
-
-function calculateEmailsPosition(maxAge) {
-    $(".circle").each(function () {
-
-        $(this).attr('data-age', function () {
-            return $(this).attr('data-age') / maxAge;
-        });
-
-        $(this).css('left', function (index) { return $(this).attr('data-age') * 700 + 'px' });
-
-        $(this).css('top', function (index) {
-
-            var top = ($(this).attr('data-from').charCodeAt(0) - "a".charCodeAt(0)) * 480 / 26;
-
-            return top + 'px'
-        });
-    })
-}
 
