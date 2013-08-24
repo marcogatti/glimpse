bdc2741274d8879afd4977c12026c281084d3f63
marcogatti <marco.gatti.90@gmail.com>
2013-08-24 18:48:44 -0300

    Empezamos con AJAX. Mails serializados, Jsonizados, y enviables. Por algun motivo mistico uno no puede Jsonmizar directamente las entidades generadas con NHibernate

--- glimpse/Controllers/AsyncMailsController.cs
+++ glimpse/Controllers/AsyncMailsController.cs
@@ -1,5 +1,9 @@
 ﻿using ActiveUp.Net.Mail;
+using Glimpse.DataAccessLayer.Entities;
+using Glimpse.Helpers;
 using Glimpse.MailInterfaces;
+using Glimpse.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +12,7 @@ using System.Web.Mvc;
 
 namespace Glimpse.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class AsyncMailsController : Controller
     {
         //
@@ -16,17 +20,55 @@ namespace Glimpse.Controllers
         public ActionResult InboxMails(int amountOfEmails = 0)
         {
 
-            AccountInterface account = (AccountInterface)Session[AccountController.MAIL_INTERFACE];
+            AccountInterface accountInterface = (AccountInterface)Session[AccountController.MAIL_INTERFACE];
 
-            if (account != null)
+            if (accountInterface != null)
             {
-                return Json(account.GetInboxMessages());
+                MailAccount mailAccount = MailAccount.FindByAddress(new CookieHelper().getMailAddressFromCookie());
+
+                MailManager manager = new MailManager(accountInterface, mailAccount);
+
+                IList<MailEntity> mails = manager.FetchFromMailbox("INBOX");
+
+                JsonResult response = Json(new { success = true, mails = PrepareToSend(mails) }, JsonRequestBehavior.AllowGet);
+                return response;
             }
             else
             {
-                return Json(new { success = false, message = "Uuups, something went wrong!" });
+                return Json(new { success = false, message = "Uuups, something went wrong!" }, JsonRequestBehavior.AllowGet);
             }
 
+        }
+
+        private IList<Object> PrepareToSend(IList<MailEntity> mails)
+        {
+            IList<Object> preparedMails = new List<Object>();
+
+            foreach (MailEntity mail in mails)
+            {
+                Object anEmail = new
+                {
+                    id = mail.Id,
+                    subject = mail.Subject,
+                    date = mail.Date,
+                    from = new
+                    {
+                        address = mail.From.MailAddress,
+                        name = mail.From.Name
+                    },
+                    to = mail.ToAddr,
+                    cc = mail.CC,
+                    bcc = mail.BCC,
+                    body = mail.Body,
+                    tid = mail.Gm_tid,
+                    seen = mail.Seen,
+                    flagged = mail.Flagged
+                };
+
+                preparedMails.Add(anEmail);
+            }
+
+            return preparedMails;
         }
 
     }
