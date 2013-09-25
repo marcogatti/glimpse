b468a0f36ae460d4522b9d7de013817659c62ecb
marcogatti <marco.gatti.90@gmail.com>
2013-09-25 11:32:30 -0300

    Se borra el viejo log, perdon eze

--- Glimpse/Models/Log.cs
+++ Glimpse/Models/Log.cs
@@ -9,24 +9,8 @@ using System.Web;
 
 namespace Glimpse.Models
 {
-    public class Log
+    public static class Log
     {
-        public LogEntity Entity { get; private set; }
-
-        public Log(LogEntity entity)
-        {
-            this.Entity = entity;
-        }
-        public void Save()
-        {
-            ISession session = NHibernateManager.OpenSession();
-            ITransaction tran = session.BeginTransaction();
-            session.SaveOrUpdate(this.Entity);
-            tran.Commit();
-            session.Flush();
-            session.Close();
-        }
-
         public static void LogException(Exception exc, String contextualMessage = null)
         {
             Exception exceptionToLog;
