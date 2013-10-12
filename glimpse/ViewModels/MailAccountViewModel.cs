0703eab607b9c4da191051a5f7aff1479243e83c
GermanPaz <paz.german.i@gmail.com>
2013-10-12 01:05:41 -0300

    Agregado el viewModel pra el MailAccount

---
+++ glimpse/ViewModels/MailAccountViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Glimpse.ViewModels
+{
+    public class MailAccountViewModel
+    {
+        public String Address { get; set; }
+        public String Password { get; set; }
+        public String ConfirmationPassword { get; set; }
+        public Boolean IsMainAccount { get; set; }
+    }
+}
