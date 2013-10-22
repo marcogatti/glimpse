958df5e2dcfaab07f8232345dea4d936c827f710
EzequielLopez <ezequiel.lopez.2009@hotmail.com>
2013-10-22 01:02:10 -0300

    El elmah loguea los mensajes de GlimpseExceptions

--- Glimpse/Models/Log.cs
+++ Glimpse/Models/Log.cs
@@ -1,6 +1,7 @@
 ﻿using Elmah;
 using Glimpse.DataAccessLayer;
 using Glimpse.DataAccessLayer.Entities;
+using Glimpse.Exceptions;
 using NHibernate;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,22 @@ namespace Glimpse.Models
 {
     public static class Log
     {
+        public static void LogException(GlimpseException exc, String contextualMessage = null)
+        {
+            Exception exceptionToLog;
+            String logMessage = "";
+
+            if (!String.IsNullOrEmpty(exc.GlimpseMessage))
+                logMessage = exc.GlimpseMessage;
+
+            if (String.IsNullOrEmpty(contextualMessage))
+                logMessage += contextualMessage;
+
+            exceptionToLog = new Exception(logMessage, exc);
+
+            Elmah.ErrorLog.GetDefault(null).Log(new Error(exceptionToLog));
+        }
+
         public static void LogException(Exception exc, String contextualMessage = null)
         {
             Exception exceptionToLog;
@@ -22,6 +39,5 @@ namespace Glimpse.Models
 
             Elmah.ErrorLog.GetDefault(null).Log(new Error(exceptionToLog));
         }
-
     }
 }
